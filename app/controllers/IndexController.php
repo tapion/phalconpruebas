6be066dbe8810b1c6759a543817ab5e81e82aac8
Miguel Angel Vargas Cabezas <puntadelanza86@gmail.com>
2013-09-10 00:07:00 -0500

    Cargue inicial

---
+++ app/controllers/IndexController.php
@@ -0,0 +1,14 @@
+<?php
+
+class IndexController extends Phalcon\Mvc\Controller 
+{
+
+	public function indexAction()
+	{
+		//echo 'hola';
+
+	}
+	public function pruebaAction(){
+		echo 'esto es una prueba';
+	}
+}
